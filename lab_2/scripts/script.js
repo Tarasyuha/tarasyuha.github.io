74587d4e8cd127802c602cfd7aca3ad4d3f03e92
FIRST_NAME LAST_NAME <galaxyxoma@gmail.com>
2023-02-02 03:30:31 -0500

    all done

--- lab_2/scripts/script.js
+++ lab_2/scripts/script.js
@@ -3,8 +3,10 @@ function changeColor() {
   var col2 = document.getElementById("colorh1");
   if (col.style.backgroundColor === "lightblue") {
     col.style.backgroundColor = "coral";
+    col.style.boxShadow = "0 0 120px coral";
   }else {
     col.style.backgroundColor = "lightblue";
+    col.style.boxShadow = "0 0 120px lightblue";
   }
   if (col.style.backgroundColor === "lightblue") {
     col2.style.color = "coral";
@@ -33,9 +35,55 @@ function hidetext() {
 function mouseOver()
 {
     backup = document.getElementById("changetext").innerHTML
+    originalColor = document.getElementById("changetext").style.boxShadow;
     document.getElementById("changetext").innerHTML = "New text WOW!";
+    document.getElementById("changetext").style.boxShadow = "0 0 90px white";
 }
 function mouseOut()
 {
-    document.getElementById("changetext").innerHTML = backup
+    document.getElementById("changetext").style.boxShadow = originalColor;
+    document.getElementById("changetext").innerHTML = backup;
+}
+
+
+
+
+
+//Button 1
+function mouseDown1() {
+  pershe1 = document.getElementById("button1").style.color;
+  druhe1 = document.getElementById("button1").style.boxShadow;
+  document.getElementById("button1").style.color = "brown";
+  document.getElementById("button1").style.boxShadow = "0 0 110px #ff9e62";
+}
+
+function mouseUp1() {
+  document.getElementById("button1").style.color = pershe1;
+  document.getElementById("button1").style.boxShadow = druhe1;
+}
+
+//Button 2
+function mouseDown2() {
+  pershe2 = document.getElementById("button2").style.color;
+  druhe2 = document.getElementById("button2").style.boxShadow;
+  document.getElementById("button2").style.color = "brown";
+  document.getElementById("button2").style.boxShadow = "0 0 110px #ff9e62";
+}
+
+function mouseUp2() {
+  document.getElementById("button2").style.color = pershe2;
+  document.getElementById("button2").style.boxShadow = druhe2;
+}
+
+//Button 3
+function mouseDown3() {
+  pershe3 = document.getElementById("button3").style.color;
+  druhe3 = document.getElementById("button3").style.boxShadow;
+  document.getElementById("button3").style.color = "brown";
+  document.getElementById("button3").style.boxShadow = "0 0 110px #ff9e62";
+}
+
+function mouseUp3() {
+  document.getElementById("button3").style.color = pershe3;
+  document.getElementById("button3").style.boxShadow = druhe3;
 }
